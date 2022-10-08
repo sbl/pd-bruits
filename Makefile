e5763165de7ae2e2b1431e009659d51a6948e8d3
Stephen Lumenta <stephen.lumenta@gmail.com>
2022-10-08 12:50:06 +0200

    some tests

--- Makefile
+++ Makefile
@@ -1,6 +1,5 @@
 # Makefile for bruits
 
-
 DEPS = ./deps
 
 lib.name = bruits
@@ -12,3 +11,9 @@ gendy~.class.sources = $(DEPS)/mt19937ar/mt19937ar.c
 datafiles = ross~-help.pd gendy~-help.pd
 
 include Makefile.pdlibbuilder
+
+.PHONY: test
+test:
+	$(CC) $(cflags) test_bruits.c deps/unity/unity.c -o $@
+	./$@
+
---
+++ test_bruits.c
@@ -0,0 +1,45 @@
+#include "unity/unity.h"
+
+#include "bruits.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_minimum(void)
+{
+
+    TEST_ASSERT_EQUAL_UINT8(2, br_minimum(2, 4));
+    TEST_ASSERT_EQUAL_FLOAT(2., br_minimum(2., 4.));
+    TEST_ASSERT_EQUAL_FLOAT(9.1, br_minimum(9.1, 13));
+}
+
+void test_maximum(void)
+{
+    TEST_ASSERT_EQUAL_UINT8(4, br_maximum(2, 4));
+    TEST_ASSERT_EQUAL_FLOAT(4., br_maximum(2., 4.));
+    TEST_ASSERT_EQUAL_FLOAT(13, br_maximum(9.1, 13));
+}
+
+void test_clamp(void)
+{
+    TEST_ASSERT_EQUAL_UINT8(0, br_clamp(-23, 0, 1));
+    TEST_ASSERT_EQUAL_UINT8(1, br_clamp(23, 0, 1));
+
+    TEST_ASSERT_EQUAL_FLOAT(0.1, br_clamp(-23, 0.1, 23.));
+    TEST_ASSERT_EQUAL_FLOAT(3.14, br_clamp(3.14, 0.1, 23.));
+    TEST_ASSERT_EQUAL_FLOAT(23, br_clamp(68, 0.1, 23.));
+}
+
+int main()
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_minimum);
+    RUN_TEST(test_maximum);
+    RUN_TEST(test_clamp);
+    return UNITY_END();
+}
